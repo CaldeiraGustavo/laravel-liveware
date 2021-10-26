6d4249a5e486d3c5ccf84e7a7353aafbe28fe615
Gustavo Caldeira <gustavoc.lightbase@sebraemg.com.br>
2021-10-26 17:43:36 -0300

    aula 5

---
+++ resources/views/livewire/show-tweets.blade.php
@@ -0,0 +1,7 @@
+<div>
+    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    Show tweets
+<p>{{$message}}</p>
+
+<input type="text" name="message" id="message" wire:model="message">
+</div>
