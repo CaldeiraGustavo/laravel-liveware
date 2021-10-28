8dc60922acf14273e6dc51a51b6cfcc1e0434698
Gustavo Caldeira <gustavoc.lightbase@sebraemg.com.br>
2021-10-27 22:21:51 -0300

    aula 14

---
+++ resources/views/livewire/user/upload-photo.blade.php
@@ -0,0 +1,17 @@
+<div>
+    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <div>
+        <h1>
+            Atualizar foto de perfil
+        </h1>
+    </div>
+    <form action="#" method="post" wire:submit.prevent="storagePhoto">
+        <input type="file" wire:model="photo">
+        <br>
+        @error('photo')
+            {{$message}}
+        @enderror
+        <br>
+        <button type="submit"> Upload Photo</button>
+    </form>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,19 @@
 use App\Http\Livewire\{
     ShowTweets
 };
+use App\Http\Livewire\User\UploadPhoto;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tweets', ShowTweets::class)->middleware('auth');
+Route::get('/tweets', ShowTweets::class)
+            ->middleware('auth')
+            ->name('upload.photo.user');
+Route::get('/upload', UploadPhoto::class)
+            ->middleware('auth')
+            ->name('tweets.index');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
