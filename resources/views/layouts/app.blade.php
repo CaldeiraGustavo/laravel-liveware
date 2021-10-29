5a0457ae6f2cf77efa982f61c1a097f0c1e79881
Gustavo Caldeira <gustavoc.lightbase@sebraemg.com.br>
2021-10-28 21:52:38 -0300

    Finalização do projeto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,21 +17,12 @@
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
+        
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.0/dist/alpine.js" defer></script>
     </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            {{-- @livewire('navigation-menu') --}}
-
-            <!-- Page Heading -->
-            {{-- @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif --}}
+    <body class="font-sans antialiased bg-gray-100">
+        <div class="container mx-auto h-200">
 
             <!-- Page Content -->
             <main>
@@ -43,6 +34,4 @@
 
         @livewireScripts
     </body>
-</html>
-/body>
 </html>
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,20 +1,19 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-    Show tweets
-    <p>{{ $conteudo }}</p>
-    <form method="post" wire:submit.prevent="create">
-        <input type="text" name="conteudo" id="conteudo" wire:model="conteudo">
-        @error('conteudo')
-            {{$message}}
-        @enderror
-        <button type="submit">Criar Tweet</button>
-    </form>
+    <div>
+        <h1 class="text-4xl py-6 font-bold h-24">Tweets</h1>
+    
+        <form method="post" wire:submit.prevent="create" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8">
+            <textarea name="conteudo" id="conteudo" rows="5" placeholder="O que está pensando?" wire:model="conteudo" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('conteudo') border-red-500 @enderror"></textarea>
+            @error('conteudo') <p><span class="text-red-500">{{ $message }}</span></p> @enderror
+            <button type="submit" class="p-2 pl-6 pr-6 rounded">Criar Tweet</button>
+        </form>
 
     <hr>
 
     @foreach ($tweets as $tweet)
-    <div class="flex">
-        <div class ="w-2/8">
+    <div class="flex m-8 bg-white shadow-md rounded p-4">
+        <div class="w-1/8 pl-3 text-center">
             @if ($tweet->user->photo)
                 <img src="{{ url("storage/{$tweet->user->photo}") }}" alt="{{$tweet->user->name}}" class="rounded-full h-8 w-8">
             @else
@@ -22,18 +21,18 @@
             @endif
             {{ $tweet->user->name }} - {{ $tweet->conteudo }}
         </div>
-        <div class='w-6/8'>
+        <div class="w-7/8 pl-3 inline-block align-text-middle">
             @if ($tweet->likes()->count())
-                <a href="#" wire:click.prevent="unlike({{$tweet->id}})">Descurtir </a>
+                <a href="#" wire:click.prevent="unlike({{$tweet->id}})" class="text-red-500">Descurtir </a>
             @else
-                <a href="#" wire:click.prevent="like({{$tweet->id}})">Curtir </a>
+                <a href="#" wire:click.prevent="like({{$tweet->id}})" class="text-teal-500">Curtir </a>
             @endif
         </div>
     </div>
     @endforeach
 
     <hr>
-    <div>
+    <div class="py-12">
         {{$tweets->links()}}
     </div>
 </div>
